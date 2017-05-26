a8361cd7071ebbdbca965eccf1b487c43f01f5bf
nzhang24 <ning.zhang@intel.com>
2017-05-26 19:36:16 +0800

    update the codes

--- src/caffe/layers/detection_layer.cpp
+++ src/caffe/layers/detection_layer.cpp
@@ -13,13 +13,13 @@ void DetectionLayer<Dtype>::Reshape(
 	const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
 	//reshpe top blob
 	vector<int> box_shape(4);
-	box_shape[0] = width_; box_shape[1] = height_;
-	box_shape[2] = num_object_; box_shape[3] = coords_;
+	box_shape[0] = num_object_; box_shape[1] = width_;
+	box_shape[2] = height_; box_shape[3] = coords_;
 	top[0]->Reshape(box_shape);
 
 	vector<int> prob_shape(4);
-	prob_shape[0] = width_; prob_shape[1] = height_;
-	prob_shape[2] = num_object_; prob_shape[3] = num_class_;
+	prob_shape[0] = num_object_; prob_shape[1] = width_;
+	prob_shape[2] = height_; prob_shape[3] = num_class_+1;
 	top[1]->Reshape(prob_shape);
 }
 
@@ -41,7 +41,24 @@ void DetectionLayer<Dtype>::LayerSetUp(
   int tmp_input_count = width_ * height_ * (num_class_ + (1 + coords_) * num_object_);
   CHECK_EQ(input_count, tmp_input_count);
 }
-
+/*
+template <typename Dtype>
+inline void softmax(Dtype *input, int n)
+{
+	Dtype sum = 0;
+	Dtype largest = -FLT_MAX;
+	for (int i = 0; i < n; ++i){
+		if (input[i] > largest) largest = input[i];
+	}
+	for (int i = 0; i < n; ++i){
+		sum += exp(input[i] - largest);
+	}
+	if (sum) sum = largest + log(sum);
+	else sum = largest - 100;
+	for (int i = 0; i < n; ++i){
+		input[i] = exp(input[i] - sum);
+	}
+}*/
 template <typename Dtype>
 void DetectionLayer<Dtype>::Forward_cpu(
     const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
@@ -53,7 +70,7 @@ void DetectionLayer<Dtype>::Forward_cpu(
 		  int index = b*width_*height_*((1 + coords_)*num_object_ + num_class_);
 		  for (int i = 0; i < width_*height_; ++i) {
 			  int offset = i*num_class_;
-			  softmax_op(input_data + index + offset, num_class_, 1);
+			  softmax_op(input_data + index + offset, num_class_,1);
 		  }
 	  }
   }
@@ -63,18 +80,21 @@ void DetectionLayer<Dtype>::Forward_cpu(
 	  int col = i % width_;
 	  for (int n = 0; n < num_object_; ++n){
 		  int index = i*num_object_ + n;
-		  int p_index = width_*height_*num_class_ + i*num_class_ + n;
+		  int p_index = width_*height_*num_class_ + i*num_object_ + n;
 		  Dtype scale = input_data[p_index];
 		  int box_index = width_*height_*(num_class_ + num_object_) + (i*num_object_ + n) * 4;
-		  box_data[index] = (input_data[box_index + 0] + col) / width_; 
-		  box_data[index+1] = (input_data[box_index + 1] + row) / width_;
+		  box_data[index] = (input_data[box_index + 0] + col) / width_; //check me ,here need to multiplied by image width
+		  box_data[index+1] = (input_data[box_index + 1] + row) / width_;//check me ,here need to multiplied by image height
 		  box_data[index+2] = pow(input_data[box_index + 2], (sqrt_ ? 2 : 1));
 		  box_data[index+3] = pow(input_data[box_index + 3], (sqrt_ ? 2 : 1));
+		  Dtype max_prob = 0;
 		  for (int j = 0; j < num_class_; ++j){
 			  int class_index = i*num_class_;
 			  Dtype prob = scale*input_data[class_index + j];
-			  prob_data[index+j] = (prob > thresh_) ? prob : 0;
+			  prob_data[index*(num_class_+1) + j] = (prob > thresh_) ? prob : 0;
+			  if (prob > max_prob) max_prob = prob;
 		  }
+		  prob_data[index*(num_class_ + 1) + num_class_] = max_prob>thresh_ ? max_prob : 0;
 	  }
   }
 }
